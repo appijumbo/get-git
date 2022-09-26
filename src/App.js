5d4e71efcea843730c73f9de4bd3a77846a37667
Tom Ormiston <tom@appijumbo.com>
2022-09-26 01:02:50 +0100

    improved ui: avatar displays with link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Search from "./components/Search";
 import RepoList from "./components/RepoList";
@@ -7,10 +7,10 @@ function App() {
   const [list, setList] = useState({});
 
   return (
-    <>
+    <div className="pageWrapper">
       <Search setList={setList} />
       <RepoList list={list} />
-    </>
+    </div>
   );
 }
 
--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -5,10 +5,19 @@ const RepoList = ({ list }) => {
   let listObj = list["items"];
   let listArray = [];
 
+  console.log(listObj);
+
   for (const theRepo in listObj) {
+    // (async () => {
+    //   const ownerUrl = listObj[theRepo].owner.url;
+    //   const getUrl = await fetch(ownerUrl);
+    //   const contentUrl = await getUrl.json();
+    //   const ownerName = await contentUrl.name;
+    //   console.log(ownerName);
+
     listArray.push({
       node_id: listObj[theRepo].node_id,
-      name: listObj[theRepo].name,
+      repoName: listObj[theRepo].name,
       login: listObj[theRepo].owner.login,
       html_url: listObj[theRepo].owner.html_url,
       avatar_url: listObj[theRepo].owner.avatar_url,
@@ -21,20 +30,25 @@ const RepoList = ({ list }) => {
         listObj[theRepo].name +
         "#readme",
     });
+    // })();
   }
-
-  console.log(listArray);
 
   const theList = listArray.map((item) => {
     return (
-      <div key={item.node_id}>
+      <div key={item.node_id} className="listCard">
+        <a href={item.html_url} target="blank" className="imageLink">
+          <img
+            src={item.avatar_url}
+            alt={item.repoName}
+            className="repoAvatar"
+          />
+        </a>
         <ul>
-          <li>{item.name}</li>
-          <li>{item.login}</li>
-          <li>{item.html_url}</li>
-          <li>{item.avatar_url}</li>
-          <li>{item.description}</li>
-          <li>{item.stargazers_count}</li>
+          <li className="listName">{item.repoName}</li>
+          <li className="listLogin">{item.login}</li>
+          <li className="listHtmlUrl">{item.html_url}</li>
+          <li className="">{item.description}</li>
+          <li className="">{item.stargazers_count}</li>
           {/* README needs to be a modal */}
         </ul>
       </div>
@@ -44,7 +58,7 @@ const RepoList = ({ list }) => {
   return (
     <>
       <h1>Repo list</h1>
-      {theList}
+      <div className="listWrapper">{theList}</div>
     </>
   );
 };
